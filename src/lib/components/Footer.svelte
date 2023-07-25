8f4a4f7def5e9bceae7a8fc3ab82328be51380a5
TinyAntFounder <137912334+TinyAntFounder@users.noreply.github.com>
2023-07-25 03:11:15 +0100

    Mint Page + WL Update

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer>
 	<div class="bg-mauve">
 		<div class="container py-8 flex justify-center items-center">
-			<span class="text-lg text-center">Tiny Ants @ 2023. All rights reserved.</span>
+			<span class="text-lg text-center">Tiny Ants @ 2023. All rights reserved.</span>				
 		</div>
 	</div>
 </footer>
