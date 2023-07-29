b84f40e494e37db95b9c5be8caaae6b2f974fc13
TinyAntFounder <137912334+TinyAntFounder@users.noreply.github.com>
2023-07-29 17:37:23 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import { slide } from 'svelte/transition';
 	import { AntType, whitelist } from './constants';
 
-	const mintDate = new Date('2023-07-29T18:00:00.000Z').getTime();
+	const mintDate = new Date('2023-07-29T17:00:00.000Z').getTime();
 	let now = new Date().getTime();
 	let distance = mintDate - now;
 	let days = 0;
